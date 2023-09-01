1811cc05b0e6a3f13de1fbafbf269399e595879e
valitutti <120913471+valitutti@users.noreply.github.com>
2023-09-01 15:21:50 +0200

    Create step1.js

---
+++ archive/code/kinetic/3_P5/step1/step1.js
@@ -0,0 +1,23 @@
+let x = 0;
+
+function setup() {
+  createCanvas(600, 400);
+  textSize(30);
+}
+
+function draw() {
+  background(220);
+  t = frameCount/60;
+  x = x + update_linear(t, 2, 3);
+  rect(x, 81, 63, 63);
+  text(round(t, 0), width / 2, height / 2);
+}
+
+function update_linear(t, start_time, duration) {
+  if (t > start_time && t - start_time < duration) {
+    return 1;
+  } else {
+    return 0;
+  }
+  
+}
