4071f346644745b4f233659f0497fc1bf567e4ff
valitutti <120913471+valitutti@users.noreply.github.com>
2024-10-11 20:04:28 +0200

    Create scripts.js

---
+++ static/js/scripts.js
@@ -0,0 +1,10 @@
+// Funzioni per cambiare lingua
+function switchToItalian() {
+    document.getElementById('content-it').style.display = 'block';
+    document.getElementById('content-en').style.display = 'none';
+}
+
+function switchToEnglish() {
+    document.getElementById('content-it').style.display = 'none';
+    document.getElementById('content-en').style.display = 'block';
+}
